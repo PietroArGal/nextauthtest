96ab460cc3ff03d9ebc724002f53bb253e8b22e9
PietroArGal <pietromauricioag@outlook.com>
2023-05-08 20:58:19 -0500

    login frontend core

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,157 @@
+'use client';
+
+import Button from '@/app/components/Button';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+import { Input } from '@/app/components/inputs/Input';
+import { useCallback, useEffect, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+export const AuthForm = () => {
+
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') {
+            setVariant('REGISTER');
+        } else {
+            setVariant('LOGIN');
+        }
+    }, [variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        if (variant === 'REGISTER') {
+
+        }
+
+        if (variant === 'LOGIN') {
+
+        }
+    }
+
+    const socialAction = (action: string) => {
+        setIsLoading(true);
+
+    }
+
+    return (
+        <div
+            className="
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+        ">
+            <div className="
+        bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10
+        ">
+                <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                    {variant === 'REGISTER' && (
+                        <Input
+                            disabled={isLoading}
+                            register={register}
+                            errors={errors}
+                            required
+                            id="name"
+                            label="Nombre"
+                        />
+                    )}
+                    <Input
+                        disabled={isLoading}
+                        register={register}
+                        errors={errors}
+                        required
+                        id="email"
+                        label="Email"
+                        type="email"
+                    />
+                    <Input
+                        disabled={isLoading}
+                        register={register}
+                        errors={errors}
+                        required
+                        id="password"
+                        label="Contraseña"
+                        type="password"
+                    />
+                    <div>
+                        <Button disabled={isLoading} fullWidth type="submit">
+                            {variant === 'LOGIN' ? 'Ingresa' : 'Registrate'}
+                        </Button>
+                    </div>
+                </form>
+
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300" />
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Continua con
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialButton
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+                </div>
+
+                <div
+                    className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+                >
+                    <div>
+                        {variant === 'LOGIN' ? 'Nuevo en Cachuelos?' : 'Ya tienes una cuenta?'}
+                    </div>
+                    <div
+                        onClick={toggleVariant}
+                        className="underline cursor-pointer"
+                    >
+                        {variant === 'LOGIN' ? 'Crea una cuenta' : 'Ingresa ahora'}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
