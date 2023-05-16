00133dfa2307bdc70296f876ced77e654a2190fb
PietroArGal <pietromauricioag@outlook.com>
2023-05-15 20:25:32 -0500

    xs

--- app/home/components/HomeHeader.tsx
+++ app/home/components/HomeHeader.tsx
@@ -3,16 +3,17 @@
 import { Fragment, useState } from 'react'
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
 
-import { HiOutlinePlusCircle, HiMenu, HiX, HiChevronDown } from 'react-icons/hi';
+import { HiOutlineLocationMarker, HiMenu, HiX, HiChevronDown, HiOutlineBriefcase, HiOutlineClipboardList, HiOutlineIdentification, HiOutlineLogin } from 'react-icons/hi';
 
-const products = [
-    { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: HiOutlinePlusCircle },
-    { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: HiOutlinePlusCircle },
+const options = [
+    { name: 'Historial De Servicios', description: 'Get a better understanding of your traffic', href: '#', icon: HiOutlineClipboardList },
+    { name: 'Editar Mis Datos', description: 'Build strategic funnels that will convert', href: '#', icon: HiOutlineIdentification },
+    { name: 'Deseo Cachuelear', description: 'Build strategic funnels that will convert', href: '#', icon: HiOutlineBriefcase },
 ]
 
 const callsToAction = [
-    { name: 'Watch demo', href: '#', icon: HiOutlinePlusCircle },
-    { name: 'Contact sales', href: '#', icon: HiOutlinePlusCircle },
+    { name: 'Cambiar Ciudad', href: '#', icon: HiOutlineLocationMarker },
+    { name: 'Cerrar Sesión', href: '#', icon: HiOutlineLogin },
 ]
 
 function rotationChevronDown(...classes: string[]) {
@@ -28,8 +29,8 @@ export const HomeHeader = () => {
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                        <span className="sr-only">Cachuelos</span>
+                        <img className="h-8 w-auto" src="https://raw.githubusercontent.com/PietroArGal/cachuelos/main/public/images/logo.png" alt="Cachuelos" />
                     </a>
                 </div>
                 <div className="flex lg:hidden">
@@ -38,14 +39,22 @@ export const HomeHeader = () => {
                         className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                         onClick={() => setMobileMenuOpen(true)}
                     >
-                        <span className="sr-only">Open main menu</span>
+                        <span className="sr-only">Abrir Menú</span>
                         <HiMenu className="h-6 w-6" aria-hidden="true" />
                     </button>
                 </div>
                 <Popover.Group className="hidden lg:flex lg:gap-x-12">
+
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        Categorías
+                    </a>
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        Buscador
+                    </a>
+
                     <Popover className="relative">
                         <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                            Product
+                            Pietro Aramburú
                             <HiChevronDown className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
                         </Popover.Button>
 
@@ -58,9 +67,9 @@ export const HomeHeader = () => {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                         >
-                            <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                            <Popover.Panel className="absolute -left-40 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                                 <div className="p-4">
-                                    {products.map((item) => (
+                                    {options.map((item) => (
                                         <div
                                             key={item.name}
                                             className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
@@ -94,19 +103,10 @@ export const HomeHeader = () => {
                         </Transition>
                     </Popover>
 
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Features
-                    </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Marketplace
-                    </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Company
-                    </a>
                 </Popover.Group>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Log in <span aria-hidden="true">&rarr;</span>
+                        ¿Necesitas Ayuda? <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
             </nav>
@@ -115,7 +115,7 @@ export const HomeHeader = () => {
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                            <span className="sr-only">Cachuelos</span>
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -127,63 +127,60 @@ export const HomeHeader = () => {
                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
                             onClick={() => setMobileMenuOpen(false)}
                         >
-                            <span className="sr-only">Close menu</span>
+                            <span className="sr-only">Cerrar Menu</span>
                             <HiX className="h-6 w-6" aria-hidden="true" />
                         </button>
                     </div>
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Categorías
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Buscador
+                                </a>
                                 <Disclosure as="div" className="-mx-3">
                                     {({ open }) => (
                                         <>
                                             <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
-                                                Product
+                                                <span className="flex items-center">Pietro Aramburú</span>
                                                 <HiChevronDown
                                                     className={rotationChevronDown(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
                                                     aria-hidden="true"
                                                 />
                                             </Disclosure.Button>
                                             <Disclosure.Panel className="mt-2 space-y-2">
-                                                {[...products, ...callsToAction].map((item) => (
+                                                {[...options, ...callsToAction].map((item) => (
                                                     <Disclosure.Button
                                                         key={item.name}
                                                         as="a"
                                                         href={item.href}
-                                                        className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                                        className="flex items-center rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                                     >
-                                                        {item.name}
+                                                        <span className="flex items-center">{item.name}</span>
+                                                        <item.icon className="h-5 w-5 text-black-400 ml-auto" />
                                                     </Disclosure.Button>
                                                 ))}
                                             </Disclosure.Panel>
                                         </>
                                     )}
                                 </Disclosure>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Features
-                                </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Marketplace
-                                </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Company
-                                </a>
+
+
                             </div>
                             <div className="py-6">
                                 <a
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
-                                    Log in
+                                    ¿Necesitas Ayuda?
                                 </a>
                             </div>
                         </div>
