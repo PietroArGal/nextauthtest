f2b01d62daa52860cad919dd9abde9c6a76c3a5c
PietroArGal <pietromauricioag@outlook.com>
2023-05-19 17:13:52 -0500

    as

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,7 +1,21 @@
-export async function GET() {
-  console.log("Get Request");
+import { NextRequest } from "next/server";
+
+export const runtime = "edge";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const mysearchparam = searchParams.get("mysearchparam");
+
+  console.log(mysearchparam);
+
+  return new Response(JSON.stringify({ name: "jhon" }), {
+    status: 401,
+  });
 }
 
-export async function POST() {
-  console.log("Post Request");
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  console.log("Post Request", body);
+
+  return new Response("OK");
 }
